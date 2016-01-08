b508a0178c5929babe1062142ce6f40d88247836
Stuart Urback <stuart@urback.net>
2016-01-08 10:41:51 -0700

    Complete hookup

---
+++ controls/app.js
@@ -0,0 +1,50 @@
+var knex = require('../db/knex')
+var express = require('express');
+var router = express.Router();
+
+var commands = {
+
+  read: function(req, res){
+    restaurants().select().then(function(result){
+      res.render('restaurants/index', {result: result})
+    })
+  },
+  readSpec: function(req, res){
+    restaurants().where('id', req.params.id).first().then(function(result){
+      res.render('restaurants/view', {result: result})
+    })
+  },
+  createUpdateDelete: function(req, res, type){
+    var object = {
+      name: req.body.name,
+      location: {
+        city: req.body.city,
+        state: req.body.state
+      },
+      cuisine : req.body.cuisine,
+      rating: req.body.rating,
+      img: req.body.image,
+      description: req.body.description
+    }
+    if (type === 'create'){
+      restaurants().insert(object, 'id').then(function(result){
+        res.redirect('index')
+      })
+    }else if(type =="update"){
+      restaurant().where('id', req.params.id).update(object).then(function(result){
+        res.redirect('index')
+      })
+    }else {
+      restaurants().where('id', req.params.id).del().then(function(result){
+        res.redirect('index');
+      });
+    }
+  }
+
+}
+
+module.exports = commands;
+
+function restaurants(){
+  return knex('mexican');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var knex = require('../db/knex');
+var controller = require('../controls/app')
 var router = express.Router();
 
+
 /* GET home page. */
+
+//Read request all
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  controller.read(req, res);
 });
 
+//Read request one
+router.get('restaurant/:id', function(req, res, next){
+  controller.readspec(req, res);
+})
+
+//Create new, form
+router.get('/restaurant/new', function(req, res, next){
+  res.render('restaurants/new');
+})
+
+//Post request Create
+router.post('/restaurant/:id', function(req, res, next){
+  if (req.body.input === 'Delete'){
+    controller.createUpdateDelete(req, res, "delete")
+  }else{
+    controller.createUpdateDelete(req, res, "create");
+  }
+});
+
+//Post request Update
+router.post('/:id/edit', function(req, res){
+  controller.createUpdateDelete(req, res, "update");
+})
+
+
 module.exports = router;
+
+
+function restaurants(){
+  return knex('mexican');
+}
