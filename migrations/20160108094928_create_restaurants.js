6617b6545235726a03f7e6aca8967cca482715d7
Stuart Urback <stuart@urback.net>
2016-01-15 10:43:40 -0700

    Working Copy

--- migrations/20160108094928_create_restaurants.js
+++ migrations/20160108094928_create_restaurants.js
@@ -1,16 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('mexican', function(table){
+  return knex.schema.createTable('locations', function(table){
     table.increments();
     table.string('name');
     table.json('location');
     table.string('cuisine');
     table.integer('rating');
     table.string('img');
-    table.string('description');
+    table.string('city');
+    table.string('state');
+    table.text('description');
   })
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('locations')
 };
---
+++ migrations/20160115102036_create_reviews.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('reviews', function(table){
+    table.increments();
+    table.string('reviewer_name')
+    table.date('date')
+    table.integer('rating')
+    table.integer('restaurant_id')
+    table.text('review')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('reviews');
+};
