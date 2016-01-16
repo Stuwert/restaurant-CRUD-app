f246a80146c1894e9a04a0a010a32f32ad61a268
Stuart Urback <stuart@urback.net>
2016-01-16 01:22:45 -0700

    CRUD routes for restaurant complete

--- controls/admincontroller.js
+++ controls/admincontroller.js
@@ -16,13 +16,51 @@ var commands = {
   },
 
   readRestaurant: function(req, res){
+    console.log(req.params);
     Restaurants().where('id', req.params.id).first().then(function(result){
       Employees().where('restaurant_id', req.params.id).then(function(resultz){
         res.render('admin/restaurantview', {restaurant: result, employees: resultz})
       })
     })
-  }
+  },
 
+  createUpdateRestaurant: function(req, res){
+    Restaurants().where('id', req.params.id).first().then(function(result){
+      res.render('admin/newrestaurant', {restaurant: result, states: states, cuisine: cuisine})
+    })
+  },
+  createNewRestaurant: function(req, res){
+    Restaurants().insert({
+      name: req.body.name,
+      city: req.body.city,
+      state: req.body.state,
+      description: req.body.description,
+      img: req.body.img,
+      rating: 0,
+      location: null,
+    }, 'id').then(function(result){
+      console.log(result)
+      res.redirect('/admin/')
+    })
+  },
+  updateRestaurant: function(req, res){
+    Restaurants().where('id', req.params.id).update({
+      name: req.body.name,
+      city: req.body.city,
+      state: req.body.state,
+      description: req.body.description,
+      img: req.body.img,
+      rating: 0,
+      location: null,
+    }).then(function(result){
+      res.redirect('/admin')
+    })
+  },
+  deleteRestaurant: function(req, res){
+    Restaurants().where('id', req.params.id).delete().then(function(){
+      res.redirect('/admin')
+    })
+  }
 
 }
 
