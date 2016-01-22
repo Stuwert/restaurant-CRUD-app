a1a39931d8f54ab7c9ddda1ea156bce180368ce3
Stuart Urback <stuart@urback.net>
2016-01-22 11:07:34 -0700

    Correct migrats and seeds

--- seeds/seed_a_neighborhoods.js
+++ seeds/seed_a_neighborhoods.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
       knex('neighborhoods').del(),
 
       // Inserts seed entries
-      knex('neighborhoods').insert({name: "Sumner Valley", epicenter: {lat: 47.2305791, lng: -122.4274978}, county: "Pierce County", city: "Sumner" , state: "WA" }),
+      knex('neighborhoods').insert({name: "Burnsville", epicenter: {lat: 44.7439681, lng: -93.2907666}, county: "Dakota County", city: "Burnsville" , state: "MN" }),
       knex('neighborhoods').insert({name: "Denver", epicenter: {lat: 39.7612652, lng: -105.003157}, county: "Denver County", city: "Denver", state: "CO"}),
       knex('neighborhoods').insert({name: "Suncadia", epicenter: {lat:47.208469 , lng: -121.0217707}, county: "Kittitas County", city: "Suncadia", state: "WA" })
     );
--- seeds/seed_b_locations.js
+++ seeds/seed_b_locations.js
@@ -9,9 +9,9 @@ exports.seed = function(knex, Promise) {
       knex('locations').del(),
 
       // Inserts seed entries
-      knex('locations').insert({name:'Yum Yum Good Times', cuisine: 'mexican', img:'/images/mexican.png', street1: '12417 12th Street East', epicenter: {lat: 47.246763, lng: -122.2646683}, street2: null, city: 'Edgewood', zip: '98372', state: 'WA', neighborhood_id: neighborhoods[1].id, description: 'An eclectic mix of flavors.'}),
-      knex('locations').insert({name:'Everything is Awesome', cuisine: 'thai', img:'/images/thai.jpg', street1: '12523 12th Street East', epicenter: {lat: 47.246739, lng: -122.2635883}, street2: null, city: 'Edgewood', zip: '98372', state: 'WA', neighborhood_id: neighborhoods[1].id, description: 'Wonderful little wine bar.'}),
-      knex('locations').insert({name:'Binga Banga', cuisine: 'italian', img:'/images/italian.png', street1: '2715 17th Street', epicenter: {lat: 39.761515, lng: -105.0126473}, street2: null, city: 'Denver', zip: '80211', state: 'CO', neighborhood_id: neighborhoods[0].id, description: 'Awesome flavors.'})
+      knex('locations').insert({name:'Yum Yum Good Times', cuisine: 'mexican', img:'/images/mexican.png', street1: '300 North College Street', epicenter: {lat: 44.461333, lng: -93.157795}, street2: null, city: 'Northfield', zip: '55057', state: 'MN', neighborhood_id: neighborhoods[0].id, description: 'An eclectic mix of flavors.'}),
+      knex('locations').insert({name:'Everything is Awesome', cuisine: 'thai', img:'/images/thai.jpg', street1: '14800 Buck Hill Rd', epicenter: {lat: 44.7331015, lng: -93.2939347}, street2: null, city: 'Burnsville', zip: '55306', state: 'MN', neighborhood_id: neighborhoods[0].id, description: 'Wonderful little wine bar.'}),
+      knex('locations').insert({name:'Binga Banga', cuisine: 'italian', img:'/images/italian.png', street1: '2715 17th Street', epicenter: {lat: 39.761515, lng: -105.0126473}, street2: null, city: 'Denver', zip: '80211', state: 'CO', neighborhood_id: neighborhoods[1].id, description: 'Awesome flavors.'})
 
     );
   })
