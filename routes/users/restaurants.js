8419e3dda33e9afcda3447b7e081c6d2d1d665ba
Stuart Urback <stuart@urback.net>
2016-01-25 09:19:21 -0700

    Refine route file

--- routes/users/restaurants.js
+++ routes/users/restaurants.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var controller = require('../../controls/restaurantcontroller')
 var router = express.Router();
-var holderobj;
 
 /* GET home page. */
 
@@ -10,57 +9,22 @@ var holderobj;
 router.get('/', controller.renderAllRestaurants);
 
 // Post a new Review
-router.post('/:id/reviews/', function(req, res){
-  if(req.body.reviewer_name !== '' && req.body.rating !== undefined && req.body.review !== ''){
-    controller.createNewReview(req, res);
-    holderobj = undefined;
-  }else{
-    holderobj = req.body;
-    res.redirect('/restaurant/' + req.params.id + '/review/new')
-  }
-});
+router.post('/:id/reviews/', controller.createNewReview);
 
 //Read request single restaurant
 router.get('/:id/', controller.renderSingleRestaurant);
 
 //Create a new Review
-router.get('/:id/reviews/new', function(req, res){
-  if (holderobj !== undefined){
-    res.render('restaurants/review', {restaurant: req.params, review: holderobj});
-  }else{
-    controller.createEditReview(req, res);
-  }
-  holderobj = undefined;
-});
+router.get('/:id/reviews/new', controller.createEditReview);
 
 //Get form to edit a review
-router.get('/:id/reviews/:reviewid/edit', function(req, res){
-  if(holderobj !== undefined){
-    res.render('restaurants/review', {restaurant: req.params, review: holderobj});
-  }else{
-    controller.createEditReview(req, res);
-  }
-})
+router.get('/:id/reviews/:reviewid/edit', controller.createEditReview)
 
 
 //Delete a Review
 router.post('/:id/reviews/:reviewid/delete', controller.deleteReview);
 
 //Update a Review
-router.post('/:id/reviews/:reviewid', function(req, res){
-  if(req.body.reviewer_name !== '' && req.body.rating !== undefined && req.body.review !== ''){
-    controller.updateReview(req, res);
-    holderobj = undefined;
-  }else{
-    holderobj = req.body;
-    holderobj.id = req.params.reviewid;
-    res.redirect('/restaurant/' + req.params.id + '/review/' + req.params.reviewid + '/edit')
-  }
-});
+router.post('/:id/reviews/:reviewid', controller.updateReview);
 
 module.exports = router;
-
-
-function restaurants(){
-  return knex('locations');
-}
