ef0db7fb6aaa3234691f316f0fe4d20fb4f599ba
Stuart Urback <stuart@urback.net>
2016-01-25 17:56:11 -0700

    Add users db

---
+++ migrations/20160125175329_create_login.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.scehma.createTable('users', function(table){
+    table.increments();
+    table.string('user_name')
+    table.string('user_password')
+    table.integer('access_level')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
